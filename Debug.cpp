681dd37f6e8b3876d68e0ec87e735fc14b600551
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2020-11-07 20:28:20 +0100

    Debug

---
+++ Debug.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Debug.h"
+//#include "App.h"
+//#include "Input.h"
+//#include "Collisions.h"
+//#include "Log.h";
+
+Debug::Debug() : Module()
+{
+	//name.Create("debug");
+}
+
+
+Debug::~Debug()
+{
+
+}
+
+bool Debug::Awake()
+{
+	return true;
+}
+
+bool Debug::Start()
+{
+	return true;
+}
+
+bool Debug::PreUpdate()
+{
+	return true;
+}
+
+bool Debug::Update(float dt)
+{
+	bool ret = true;
+
+	
+	/*if (App->input->GetKey(SDL_SCANCODE_F9) == KEY_DOWN)
+	{
+		ToggleColliders();
+	}*/
+
+	return ret;
+}
+
+bool Debug::PostUpdate()
+{
+
+	return true;
+}
+
+bool Debug::CleanUp()
+{
+	return true;
+}
+
+void Debug::ToggleColliders()
+{
+	if (app->collisions->showColliders == false)
+	{
+		app->collisions->showColliders = true;
+	}
+	else if (app->collisions->showColliders == true) {
+		app->collisions->showColliders = false;
+	}
+}
---
+++ Debug.h
@@ -0,0 +1,41 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include "Module.h"
+
+#include "SDL/include/SDL_scancode.h"
+
+class Debug : public Module
+{
+public:
+	Debug();
+
+	// Destructor
+	virtual ~Debug();
+
+	//Called at the beginning of the application execution
+	bool Awake();
+
+	//Called when the module is activated
+	//By now we will consider all modules to be permanently active
+	bool Start();
+
+	//Called at the beginning of each application loop
+	bool PreUpdate();
+
+	//Called at the middle of each application loop
+	bool Update(float dt);
+
+	//Called at the end of each application loop
+	bool PostUpdate();
+
+	//Called at the end of the application
+	bool CleanUp();
+
+	void ToggleColliders();
+	void LoadLvl1();
+	void LoadLvL2();
+
+};
+
+#endif //__DEBUG_H__
